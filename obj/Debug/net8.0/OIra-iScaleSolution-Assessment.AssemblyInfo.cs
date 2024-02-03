5de58e30123d826b62f3a47d652cbfc342d0c510
josefoira23 <josefoira20@gmail.com>
2024-02-04 02:37:20 +0800

    Configure IIS Express run on port http 3000

--- obj/Debug/net8.0/OIra-iScaleSolution-Assessment.AssemblyInfo.cs
+++ obj/Debug/net8.0/OIra-iScaleSolution-Assessment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OIra-iScaleSolution-Assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf48d7acaeeabf187d49b4377a22380c55eb533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf73cc17df38d68c3fcf766df2150911292a202")]
 [assembly: System.Reflection.AssemblyProductAttribute("OIra-iScaleSolution-Assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OIra-iScaleSolution-Assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
